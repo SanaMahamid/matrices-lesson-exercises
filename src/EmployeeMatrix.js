0607481df8db21074f8ed4ebbf7eeff00023b4c1
sana mahamid <sanaa_6-5.7@hotmail.com>
2020-12-17 06:06:40 +0200

    done

--- src/EmployeeMatrix.js
+++ src/EmployeeMatrix.js
@@ -1,9 +1,83 @@
 /* Write your code below */
+const Matrix = require('./Matrix')
+
+class EmployeeMatrix extends Matrix{
+    constructor(){
+        super()
+    }
+
+    loadData(salaryData){
+        this.matrix = []
+        for(let i=0 ; i<salaryData.length; i++){
+            this.matrix.push(Object.values(salaryData[i]))
+        }
+    }
+
+    getEmployees(department){
+        return this.matrix.filter(r => r[2] === department).map(r => r[1])
+    }
+
+    getTotalSalary(department) {
+	    let sum = 0
+	    this.matrix.filter(row => row[2] === department)
+	      .forEach(row => sum += row[3])
+	    return sum
+      }
+      
+      findRichest() {
+	
+
+	    let highestSalary = 0
+	    let name = ''
+	
+
+	    for (let i = 0; i < this.matrix.length; i++) {
+	      if (this.matrix[i][3] > highestSalary) {
+	        name = this.matrix[i][1]
+	        highestSalary = this.matrix[i][3]
+	      }
+	    }
+	
+
+	    return name
+	  }
+
+
+}
 
 
 
 
 //You can paste the code from the lesson below to test your solution
+
+/*let data = [
+    { _id: "e10021", name: "Gillian", department: "Finance", salary: 2000 },
+    { _id: "e10725", name: "Tibor", department: "Design", salary: 1200 },
+    { _id: "e10041", name: "Anisha", department: "Finance", salary: 2300 },
+    { _id: "e10411", name: "Jakub", department: "Design", salary: 1600 },
+    { _id: "e11995", name: "Mar", department: "Design", salary: 1300 },
+    { _id: "e10732", name: "Nisha", department: "Design", salary: 1200 }
+]
+
+let m = new EmployeeMatrix()
+
+m.loadData(data)
+m.print()
+//prints
+
+/*
+e10021  Gillian Finance 2000
+e10725  Tibor   Design  1200
+e10041  Anisha  Finance 2300
+e10411  Jakub   Design  1600
+e11995  Mar     Design  1300
+e10732  Nisha   Design  1200
+*/
+/*
+console.log(m.getEmployees("Finance")) //prints [ 'Gillian', 'Anisha' ]
+console.log(m.getEmployees("Design")) //prints [ 'Tibor', 'Jakub', 'Mar', 'Nisha' ]
+*/
+
 
 
 
--- src/Matrix.js
+++ src/Matrix.js
@@ -1,9 +1,89 @@
 /* Write your code below */
+class Matrix{
+    constructor(r, c){
+        this.matrix = this.generateMatrix(r,c)
+    }
+
+    generateMatrix(numRows, numColumns) {
+        let matrix = []
+        let num = 1
+        
+        for (let r = 0; r < numRows; r++) {
+            matrix.push([])
+            for (let c = 0; c < numColumns; c++) {
+                matrix[r].push(num++)
+            }
+        }
+        return matrix
+    }
+
+    get(rowNum, colNum){
+        return this.matrix[rowNum][colNum]
+    }
+
+    print(){
+        let row = ""
+        for(let i=0; i<this.matrix.length; i++){
+            for(let j=0; j<this.matrix[i].length; j++){
+                row += this.matrix[i][j] + "\t"
+            }
+            console.log(row)
+            row = ""
+        }
+    }
+
+    printColumn(colNum) {
+        for (let i = 0; i < this.matrix.length; i++) {
+            console.log(this.matrix[i][colNum])
+        }
+    }
+    
+    printRow(rowNum){
+        let row =""
+        for (let i = 0; i < this.matrix[rowNum].length; i++) {
+            row += this.matrix[rowNum][i] + "\t"
+        }
+        console.log(row)
+    }
+        
+    alter(rowNum, colNum, value){
+        this.matrix[rowNum][colNum] = value
+    }
+
+    findCoordinate(value){
+        for(let i=0; i<this.matrix.length; i++){
+            for(let j=0; j<this.matrix[i].length; j++){
+                if(this.get(i,j) === value){
+                    return {x:j, y:i}
+                }
+            }
+        }
+        return null
+    }
+
+}
 
 
 
 
 //You can paste the code from the lesson below to test your solution
+//let m = new Matrix(3, 4)
+//m.print()
+//prints
+/*
+1       2       3       4
+5       6       7       8
+9       10      11      12
+*/
+
+/*m.alter(0, 0, m.get(1, 1))
+m.printColumn(0) //prints 6, 5, 9 (separate lines)
+m.printRow(0) //prints 6, 2, 3, 4 (separate lines)
+
+console.log(m.findCoordinate(12)) //prints {x: 3, y: 2}
+console.log(m.findCoordinate(7)) //prints {x: 2, y: 1}*/
+
+
 
 
 
